6cea77d9602598f94d75d5e846d524244995267b
Jym Bocala <jym.bocala@gmail.com>
2023-01-02 22:07:44 +1100

    add basic elements for Header component

---
+++ src/components/Header.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+export default function Header() {
+  return (
+    <header className="header">
+      <div className="hero">
+        <h1 className="hero__title">Quick Lesson Planner</h1>
+        <p className="hero__info">Welcome to Quick Lesson Planner, the web app that helps teachers save time on lesson planning. Simply add your ideas and activities and we'll generate a complete lesson plan for you. Try Quick Lesson Planner and see how much time you can save!</p>
+      </div>
+    </header>
+  )
+}
---
+++ src/components/Main.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function Main() {
+  return (
+    <section>
+      <h2>Main</h2>
+    </section>
+  )
+}
