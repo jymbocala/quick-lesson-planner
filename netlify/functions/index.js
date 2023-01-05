fca0f2b0288c356e8caee2666298e4ba4edea387
Jym Bocala <jym.bocala@gmail.com>
2023-01-05 22:27:52 +1100

    use netlify functions

--- netlify/functions/index.js
+++ netlify/functions/index.js
@@ -3,40 +3,30 @@
 const OpenAI = require("openai");
 const { Configuration, OpenAIApi } = OpenAI;
 
-const dotenv = require('dotenv');
+const dotenv = require("dotenv");
 dotenv.config();
-const express = require("express");
-const bodyParser = require("body-parser");
-const cors = require("cors");
-const app = express();
-const port = 3001;
+
 // API credentials
-const api_key = process.env.API_KEY
+const api_key = process.env.API_KEY;
 
 const configuration = new Configuration({
   organization: "org-oaUsSfZBaz6fJgT00AJcF2Dj",
-  apiKey: api_key
+  apiKey: api_key,
 });
 const openai = new OpenAIApi(configuration);
 
-app.use(bodyParser.json());
-app.use(cors());
-
-app.post("/", async (req, res) => {
-  const { message } = req.body;
+exports.handler = async function (event, context) {
+  // your server-side functionality
+  const { message } = JSON.parse(event.body);
   const response = await openai.createCompletion({
     model: "text-davinci-003",
     prompt: `${message}`,
-    max_tokens: 100,
+    max_tokens: 200,
     temperature: 0.5,
   });
-  console.log(response.data)
+  console.log(response.data);
   // When OpenAI comes back with a response, pass it to the front end.
-  if(response.data.choices[0].text) {
-    res.json({message: response.data.choices[0].text})
+  if (response.data.choices[0].text) {
+    res.json({ message: response.data.choices[0].text });
   }
-});
-
-app.listen(port, () => {
-  console.log("Example app listening", port);
-});
+};
