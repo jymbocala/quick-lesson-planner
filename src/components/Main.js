c742f92bcd39b66af46b3619e54264d0733098a7
Jym Bocala <jym.bocala@gmail.com>
2023-01-06 21:37:29 +1100

    add form elements and change message stage to lessonFormData

--- src/components/Main.js
+++ src/components/Main.js
@@ -3,10 +3,18 @@ import React, { useState } from "react";
 
 export default function Main() {
   // Initialize message and response states
-  const [message, setMessage] = useState("");
+  const [lessonFormData, setLessonFormData] = useState(
+    {
+      subject: "",
+      length: "",
+      topic: "",
+      learningIntention: "",
+      activities: ""
+    }
+  );
   const [response, setResponse] = useState("");
 
-  // Funtion to call a fetch request when Submit button is clicked
+  // Function to call a fetch request when Submit button is clicked
   function handleSubmit(e) {
     e.preventDefault(); // this stops refreshing the page when button is clicked
     console.log("button clicked!")
@@ -15,7 +23,7 @@ export default function Main() {
       headers: {
         "Content-Type": "application/json",
       },
-      body: JSON.stringify({ message }),
+      body: JSON.stringify({ lessonFormData }),
     })
       .then((res) => res.json())
       .then((data) => setResponse(data.message));
@@ -24,19 +32,62 @@ export default function Main() {
   // Function to handle change in textarea
   function handleChange(e) {
     const {value} = e.target;
-    setMessage(value)
+    setLessonFormData(value)
   }
 
   return (
     <section className="main">
+      <h2>Create a Lesson Plan</h2>
       <form className="main__form" >
+        <input 
+          type="text"
+          placeholder="Lesson Subject"
+          onChange={handleChange}
+          name="subject"
+          value={lessonFormData.subject}
+        />
+
+        <label htmlFor="length">Lesson length:</label>
+        
+        <select 
+            id="length" 
+            value={lessonFormData.length}
+            onChange={handleChange}
+            name="length"
+        >
+          <option value="30">30</option>
+          <option value="35">35</option>
+          <option value="40">40</option>
+          <option value="45">45</option>
+          <option value="50">50</option>
+          <option value="55">55</option>
+          <option value="60">60</option>
+        </select>
+
+        <input 
+          type="text"
+          placeholder="Lesson Topic"
+          onChange={handleChange}
+          name="topic"
+          value={lessonFormData.topic}
+        />
+        <input 
+          type="text"
+          placeholder="Learning Intention  (optional)"
+          onChange={handleChange}
+          name="learningIntention"
+          value={lessonFormData.learningIntention}
+        />
+        <br />
         <textarea
           className="form__textarea"
-          value={message}
+          placeholder="Enter activities"
+          value={lessonFormData.activities}
           onChange={handleChange}
+          
         />
         <button className="form__button" onClick={handleSubmit}>
-          CREATE LESSON PLAN
+          GENERATE LESSON PLAN
         </button>
       </form>
 
