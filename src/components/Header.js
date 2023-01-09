49a986c4b2d106ba39f2fff7abb904d36c195e8c
Jym Bocala <jym.bocala@gmail.com>
2023-01-10 00:27:41 +1100

    update head styling for medium to larger screens

--- src/components/Header.js
+++ src/components/Header.js
@@ -5,8 +5,10 @@ export default function Header() {
     <header className="header">
       <div className="hero">
         <img src={require("../assets/hero-sketch.svg").default} alt="girl working on laptop" className="hero__img"/>
-        <h1 className="hero__title">Quick Lesson Planner</h1>
-        <p className="hero__info">Welcome to Quick Lesson Planner, the web app that helps teachers save time on lesson planning. Simply add your ideas and activities and we'll generate a complete lesson plan for you. Try Quick Lesson Planner and see how much time you can save!</p>
+        <div className="hero__text-group">
+          <h1 className="hero__title">Quick Lesson Planner</h1>
+          <h3 className="hero__info">Welcome to Quick Lesson Planner, the web app that helps teachers save time on lesson planning. Simply add your ideas and activities and we'll generate a complete lesson plan for you. Try Quick Lesson Planner and see how much time you can save!</h3>
+        </div>
       </div>
     </header>
   )
