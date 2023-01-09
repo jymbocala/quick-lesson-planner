fc06d37dbdb414e06d8d2970d3ad06fe2d8a4732
Jym Bocala <jym.bocala@gmail.com>
2023-01-10 02:14:47 +1100

    add form instruction text & update placeholders to contain examples

--- src/components/Main.js
+++ src/components/Main.js
@@ -60,7 +60,7 @@ export default function Main() {
     if (!isLoading && response === "") {
       return <></>;
     } else if (isLoading) {
-      return <div>Loading...</div>; // TODO: create Loading component
+      return <Loader />;
     } else if (!isLoading) {
       return <Results response={response} />;
     }
@@ -71,14 +71,15 @@ export default function Main() {
       <h2>Create a Lesson Plan</h2>
       {/* TODO: add instructions in between the form input */}
       <form className="main__form">
+        <p className="form__instruction-text">1. Add lesson subject and length.</p>
         <input
           type="text"
-          placeholder="Lesson Subject"
+          placeholder={`Example: "Business Management"`}
           onChange={handleChange}
           name="subject"
           value={lessonFormData.subject}
         />
-
+        {/* TODO: update "length" naming to avoid potential bugs */}
         <div className="form_length">
           <label htmlFor="length">Lesson length: </label>
           <select
@@ -94,27 +95,30 @@ export default function Main() {
             <option value="50">50</option>
             <option value="55">55</option>
             <option value="60">60</option>
+            <option value="65">65</option>
+            <option value="70">70</option>
           </select>
           <label>minutes</label>
         </div>
-
+        <p className="form__instruction-text">2. Add main topic of the lesson and the learning intention.</p>
         <input
           type="text"
-          placeholder="Lesson Topic"
+          placeholder={`Lesson Topic example: "Motivational Theories"`}
           onChange={handleChange}
           name="topic"
           value={lessonFormData.topic}
         />
         <input
           type="text"
-          placeholder="Learning Intention"
+          placeholder={`Lesson Intention example: "Analyse how Maslow's Hierarchy of Needs can be used to motivate employees."`}
           onChange={handleChange}
           name="learningIntention"
           value={lessonFormData.learningIntention}
         />
         <br />
+        <p className="form__instruction-text">3. Add some ideas/activities for the lesson. The more detail you add - the better!</p>
         <textarea
-          placeholder="Enter activities"
+          placeholder={`Example (feel free to add dot points!): \n - 5-minute video to introduce the topic. \n - Research task where students work in pairs. \n - Discussion`}
           value={lessonFormData.activities}
           onChange={handleChange}
           name="activities"
@@ -123,9 +127,7 @@ export default function Main() {
           GENERATE LESSON PLAN
         </button>
       </form>
-      <div>
-      <Loader />  
-      </div>
+
       {/* call handleDisplayRender when app first renders */}
       {handleDisplayRender()}
     </section>
