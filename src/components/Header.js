3e0ec9fc79a9005c13383811109968ae5b046759
Jym Bocala <jym.bocala@gmail.com>
2023-01-20 22:04:55 +1100

    update hero info text

--- src/components/Header.js
+++ src/components/Header.js
@@ -7,7 +7,7 @@ export default function Header() {
         <img src={require("../assets/diary-hero-sketch.svg").default} alt="girl working on laptop" className="hero__img"/>
         <div className="hero__text-group">
           <h1 className="hero__title">Quick Lesson Planner</h1>
-          <h3 className="hero__info">Welcome to Quick Lesson Planner, the web app that helps teachers save time on lesson planning. Simply add your ideas and activities and we'll generate a complete lesson plan for you. Try Quick Lesson Planner and see how much time you can save!</h3>
+          <h3 className="hero__info">Effortlessly plan lessons with Quick Lesson Planner. Enter your ideas, get a complete plan in seconds. {<br/>} Start simplifying lesson planning today!</h3>
         </div>
       </div>
     </header>
