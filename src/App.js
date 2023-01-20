ae0154145f52325e7a034d98b0efc6a818486cdc
Jym Bocala <jym.bocala@gmail.com>
2023-01-20 21:40:47 +1100

    update theme colors

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
 import React from "react";
-import "./styles/App.scss";
-import "./styles/queries.scss";
-import "./styles/header.scss";
-import "./styles/loader.scss";
-import "./styles/footer.scss";
-
 import Header from "./components/Header";
 import Main from "./components/Main";
 import Footer from "./components/Footer";
+import "./styles/App.scss";
+import "./styles/header.scss";
+import "./styles/loader.scss";
+import "./styles/footer.scss";
+import "./styles/queries.scss";
+
 
 function App() {
   return (
--- src/components/Header.js
+++ src/components/Header.js
@@ -4,7 +4,7 @@ export default function Header() {
   return (
     <header className="header">
       <div className="hero">
-        <img src={require("../assets/hero-sketch.svg").default} alt="girl working on laptop" className="hero__img"/>
+        <img src={require("../assets/diary-hero-sketch.svg").default} alt="girl working on laptop" className="hero__img"/>
         <div className="hero__text-group">
           <h1 className="hero__title">Quick Lesson Planner</h1>
           <h3 className="hero__info">Welcome to Quick Lesson Planner, the web app that helps teachers save time on lesson planning. Simply add your ideas and activities and we'll generate a complete lesson plan for you. Try Quick Lesson Planner and see how much time you can save!</h3>
