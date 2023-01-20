c1910501424ce87cef208d8eafc3b8ba544bebe4
Jym Bocala <jym.bocala@gmail.com>
2023-01-20 19:46:00 +1100

    separate styles

--- src/App.js
+++ src/App.js
@@ -1,7 +1,9 @@
 import React from "react";
 import "./styles/App.scss";
 import "./styles/queries.scss";
+import "./styles/header.scss";
 import "./styles/loader.scss";
+import "./styles/footer.scss";
 
 import Header from "./components/Header";
 import Main from "./components/Main";
